f7620a25f9b637a4c1ce71f2af3b26fedc946865
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2022-11-02 17:40:26 -0500

    Create array of placesand printed the array's length

--- friends.rb
+++ friends.rb
@@ -4,3 +4,9 @@ arrays = ["Aaron", "Josh", "Scott", "Kelly", "Brie"]
     puts arrays[4]
     puts arrays[0]
 
+puts
+
+places = ["Paris", "Tokyo", "Hawai'i", "Canada", "Washington"]
+
+puts "There are #{places.length} places in this array"
+ 
