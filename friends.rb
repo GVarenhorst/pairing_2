6db6fa045c4f421495ccbcf2d3db01c396bd3f7b
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-02-07 17:38:08 -0600

    Add second array

--- friends.rb
+++ friends.rb
@@ -1,4 +1,11 @@
 arrays = ["Matthew", "Garrett", "Bob", "Betty", "Boop"]
-puts arrays [2]
-puts arrays.pop
-puts arrays.shift
+    puts arrays [2]
+    puts arrays.pop
+    puts arrays.shift
+
+
+
+
+places = ["Lisbon", "Maui", "Mount Evans", "White Sands", "Sand Dunes"]
+    puts places.unshift
+
